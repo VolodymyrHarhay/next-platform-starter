36eb7eafbb95c75412ff3a979dad49a9ad0de1bb
Volodymyr <volodymyr.harhay@bookedby.com>
2025-02-03 17:03:59 -0600

    Widget implementation with polling and dynamic updates

--- app/page.jsx
+++ app/page.jsx
@@ -4,6 +4,7 @@ import { RandomQuote } from 'components/random-quote';
 import { Markdown } from 'components/markdown';
 import { ContextAlert } from 'components/context-alert';
 import { getNetlifyContext } from 'utils';
+import { TestWidgetButton } from 'components/test-widget-button';
 
 const contextExplainer = `
 The card below is rendered on the server based on the value of \`process.env.CONTEXT\` 
@@ -36,36 +37,32 @@ export default function Page() {
                 >
                     Read the Docs
                 </Link>
-                {/* alaska */}
-                {/* <div data-widget="wait-time" data-token="eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJTdG9yZUlkIjoiMzAyNzc4MzUtMWQyNS00NWExLWJiYTYtOTg2MDZjYTkzYTg1IiwiQm9va2luZ0dyb3VwSWQiOiJkZDdkODA4Mi02YjFiLTQzNDQtYWIxNS05YzdlMzk5NTc1NDMiLCJEZXBsb3ltZW50LVVpZCI6ImM1OWI3NTY5LTIwMzYtODI1NC1jNjcwLWU5ZjQ2NGJhNTRmNiIsIlN0b3JlLUlkIjoiOSIsIldpZGdldC1JZCI6ImRjMDJiZTkxLTRiMjItNDM2Mi1iM2Y3LTMzY2U4OTM1ZGQwMCIsImlzcyI6IkJvb2tlZEJ5LldpZGdldCIsImF1ZCI6IkJvb2tlZEJ5In0.dDErhEveKXr_DDfOXEPEsvAlq7280FcOxG3t4mXJ1O0"></div> */}
-                {/* dmytro */}
-                {/* <div data-widget="wait-time" data-token="eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJTdG9yZUlkIjoiMDUxMjY3MmEtOWEwNC00NTNkLTkzZDctMzc1YjhkZTYzNGUwIiwiQm9va2luZ0dyb3VwSWQiOiJkZDdkODA4Mi02YjFiLTQzNDQtYWIxNS05YzdlMzk5NTc1NDMiLCJEZXBsb3ltZW50LVVpZCI6ImM1OWI3NTY5LTIwMzYtODI1NC1jNjcwLWU5ZjQ2NGJhNTRmNiIsIlN0b3JlLUlkIjoiMTAiLCJXaWRnZXQtSWQiOiI5MTI0YzJmMC00NzZiLTQ4NDktOGYwMC1hOGY3ZTlkMGY2NmMiLCJpc3MiOiJCb29rZWRCeS5XaWRnZXQiLCJhdWQiOiJCb29rZWRCeSJ9.A9vUtHBvdzVx12lunSj0VKYcTuiWDXv5gmPGttH8E1k"></div> */}
-                {/* Fantastic Sams Bolingbrook */}
-                {/* <div data-widget="wait-time" data-token="eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJTdG9yZUlkIjoiNzQ3OTdhMjgtMzE0Ni00NDhhLWFlNTQtNDJkMTg5OWFkODk2IiwiQm9va2luZ0dyb3VwSWQiOiJkZDdkODA4Mi02YjFiLTQzNDQtYWIxNS05YzdlMzk5NTc1NDMiLCJEZXBsb3ltZW50LVVpZCI6ImM1OWI3NTY5LTIwMzYtODI1NC1jNjcwLWU5ZjQ2NGJhNTRmNiIsIlN0b3JlLUlkIjoiMzEiLCJXaWRnZXQtSWQiOiI4NjIxY2RmMy1iNmY3LTQyYjUtYjE4My0zNzQ3YjNjODgxNTIiLCJpc3MiOiJCb29rZWRCeS5XaWRnZXQiLCJhdWQiOiJCb29rZWRCeSJ9.0DpWHky6mFAtWKwZWYFnppqIYCuiO-XHWpLsoHSITk0"></div> */}
+                <TestWidgetButton />
                 {/* Peca 4min* localhost*/}
-                <a 
-                  data-widget="wait-time" 
+                {/* <a
+                  data-widget="wait-time"
+                  data-use-default-styles="false"
                   data-token="eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJTdG9yZUlkIjoiYzM4MDdjZjUtMDVlOC00YjRkLTk2YzktZGM4ZTc4YTUwNWY0IiwiQm9va2luZ0dyb3VwSWQiOiJkZDdkODA4Mi02YjFiLTQzNDQtYWIxNS05YzdlMzk5NTc1NDMiLCJEZXBsb3ltZW50LVVpZCI6ImM1OWI3NTY5LTIwMzYtODI1NC1jNjcwLWU5ZjQ2NGJhNTRmNiIsIlN0b3JlLUlkIjoiNiIsIldpZGdldC1JZCI6IjI2ZjNiM2IwLWM5YjAtNDFhNC05NGMzLTFjY2E2YmMyYjNkMCIsImlzcyI6IkJvb2tlZEJ5LldpZGdldCIsImF1ZCI6IkJvb2tlZEJ5In0.gRnVp8eoepxSczcd4rIEhdTZhXKZbCdE7pm8szhkRxQ"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                </a>
+                </a> */}
                 {/* zona dsk 0min* https://bucolic-faloodeh-456aa7.netlify.app/*/}
-                <a 
+                {/* <a 
                   data-widget="wait-time" 
                   data-token="eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJTdG9yZUlkIjoiNmNjZjhlZDktNjk1Ny01N2U1LTlkNGEtMjY0YjI3Yjc5ZmJmIiwiQm9va2luZ0dyb3VwSWQiOiJkZDdkODA4Mi02YjFiLTQzNDQtYWIxNS05YzdlMzk5NTc1NDMiLCJEZXBsb3ltZW50LVVpZCI6ImM1OWI3NTY5LTIwMzYtODI1NC1jNjcwLWU5ZjQ2NGJhNTRmNiIsIlN0b3JlLUlkIjoiNCIsIldpZGdldC1JZCI6ImNiNDUwNmMwLWU3NTMtNGExNS04MzBmLTEzZTU5NDU4NjRiOSIsImlzcyI6IkJvb2tlZEJ5LldpZGdldCIsImF1ZCI6IkJvb2tlZEJ5In0.mCHdU-EIAkkHSVBV5tahKLaW2EJ-8-vAOKiLVnkOSh8"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                </a>
+                </a> */}
                 {/* Rashid's Tonsorina closed https://some-wix-test-s8t.online/*/}
-                <a 
+                {/* <a 
                   data-widget="wait-time" 
                   data-token="eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJTdG9yZUlkIjoiMzM5Yjg4YjMtOWNmZS00YTU3LTg3ZDMtNzAxMGVhYjg4YjFmIiwiQm9va2luZ0dyb3VwSWQiOiJkZDdkODA4Mi02YjFiLTQzNDQtYWIxNS05YzdlMzk5NTc1NDMiLCJEZXBsb3ltZW50LVVpZCI6ImM1OWI3NTY5LTIwMzYtODI1NC1jNjcwLWU5ZjQ2NGJhNTRmNiIsIlN0b3JlLUlkIjoiMTEiLCJXaWRnZXQtSWQiOiI1YzgxMWVkZS1mYzc0LTQ1YzktYTYyMy1iMTY4MTIwYzIyMDYiLCJpc3MiOiJCb29rZWRCeS5XaWRnZXQiLCJhdWQiOiJCb29rZWRCeSJ9.Wc0OmOmiLCzgZ9jkCHh0raBQvJ_6qS1cb2TMdsdQ3nI"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                </a>
+                </a> */}
             </section>
             {!!ctx && (
                 <section className="flex flex-col gap-4">
---
+++ components/test-widget-button.jsx
@@ -0,0 +1,28 @@
+'use client';
+
+export function TestWidgetButton() {
+    return (
+        <button 
+            onClick={() => {
+                setTimeout(() => {
+                    const widget = document.createElement('a');
+                    widget.setAttribute('data-widget', 'wait-time');
+                    widget.setAttribute('data-use-default-styles', 'false');
+                    widget.setAttribute('data-token', 'eyJhbGciOiJodHRwOi8vd3d3LnczLm9yZy8yMDAxLzA0L3htbGRzaWctbW9yZSNobWFjLXNoYTI1NiIsInR5cCI6IkpXVCJ9.eyJTdG9yZUlkIjoiYzM4MDdjZjUtMDVlOC00YjRkLTk2YzktZGM4ZTc4YTUwNWY0IiwiQm9va2luZ0dyb3VwSWQiOiJkZDdkODA4Mi02YjFiLTQzNDQtYWIxNS05YzdlMzk5NTc1NDMiLCJEZXBsb3ltZW50LVVpZCI6ImM1OWI3NTY5LTIwMzYtODI1NC1jNjcwLWU5ZjQ2NGJhNTRmNiIsIlN0b3JlLUlkIjoiNiIsIldpZGdldC1JZCI6IjQ1ZjcyZDFkLTBjNDctNDdmMS1hYmM0LWMyYjNhNmU5YTUxOCIsImlzcyI6IkJvb2tlZEJ5LldpZGdldCIsImF1ZCI6IkJvb2tlZEJ5In0.nmtIoFPZCYF56WZRUIGv9rv0KvvpppkMr-vx87w397A');
+                    widget.setAttribute('target', '_blank');
+                    widget.setAttribute('rel', 'noopener noreferrer');
+                    
+                    const section = document.querySelector('.flex.flex-col.items-start.gap-3.sm\\:gap-4');
+                    if (section) {
+                        section.appendChild(widget);
+                    } else {
+                        console.error('Section not found');
+                    }
+                }, 2000);
+            }}
+            className="btn btn-lg btn-secondary sm:btn-wide"
+        >
+            Add Widget After 2s
+        </button>
+    );
+}
